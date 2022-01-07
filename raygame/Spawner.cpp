d520e0a1a8c9e4304dea8110b40659d136f59ca3
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2022-01-07 10:58:47 -0600

    Extra save Spawner

--- raygame/Spawner.cpp
+++ raygame/Spawner.cpp
@@ -10,11 +10,20 @@ void Spawner::update(float deltaTime)
 
 	if (m_numberofenemies < 10 && m_timesincelastSpawn > m_spawnCooldown)
 	{
+		//Declares the enemy that wil be spawned
 			m_enemyspawn = new Enemy(10, 10, "Taco", "Rusher", Spawner::m_currentplayer);
 			m_enemyspawn->getTransform()->setScale({ 50,50 });
 
+			//Adds that enemy to the game
 			Engine::getCurrentScene()->addActor(m_enemyspawn);
-			m_numberofenemies++;
-			m_timesincelastSpawn = 0;			
+			m_numberofenemies++;//Adds to the number of enemys
+			m_timesincelastSpawn = 0;//resets the time since last spawn	
+
+			int randomNumber;
+			for (int index = 0; index < 10; index++)
+			{
+				randomNumber = (rand() % 35) + 1;
+			}			
+		   
 	}
 }
